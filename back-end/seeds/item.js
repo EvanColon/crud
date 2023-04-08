1d03e070b81c5310da286784ad917f2205eb83a9
Evan Colon <evan.colon98@gmail.com>
2023-04-08 07:06:00 -0500

    last edit

--- back-end/seeds/item.js
+++ back-end/seeds/item.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('item').del()
   await knex('item').insert([
-    {userId: 1, itemName: 'Nikes', description: 'Classy athletic footwear', quantity: 1},
+    {userId: 1, itemName: 'Nikes', description: 'Classy', quantity: 1},
     {userId: 2, itemName: 'Jordans', description: 'Fire', quantity: 3}
   ]);
 };
